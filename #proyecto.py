73da6e355831e89936e4f88aa6ea2b13d7a04438
jimm-volition <jui.mendoza@duocuc.cl>
2025-06-20 18:46:23 -0400

    le puse un def nuevo

--- #proyecto.py
+++ #proyecto.py
@@ -1,2 +1,12 @@
+#proyecto
+print("")
+print("Bienvenido al programa de gestión de proyectos")
+print("Por favor, ingrese el nombre del proyecto:")
+def menu():
+    print("1. Crear proyecto")
+    print("2. Ver proyectos")
+    print("3. Salir")
+    opcion = input("Seleccione una opción: ")
+    return opcion
 def menu1():
     print('Este es el menu 1')

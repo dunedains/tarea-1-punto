152d31792de3ff684c45c1380e2b6c91fdd8524e
dunedains <fe.zapatao@duocuc.cl>
2025-06-20 18:27:12 -0400

    le agregue un def

--- #proyecto.py
+++ #proyecto.py
@@ -2,3 +2,9 @@
 print("")
 print("Bienvenido al programa de gestión de proyectos")
 print("Por favor, ingrese el nombre del proyecto:")
+def menu():
+    print("1. Crear proyecto")
+    print("2. Ver proyectos")
+    print("3. Salir")
+    opcion = input("Seleccione una opción: ")
+    return opcion
